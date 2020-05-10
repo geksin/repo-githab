cfa89de14f8ae668ee20ab0edf2db03e6d05685c
Dmitrii <dimon_men@bk.ru>
2020-05-10 20:35:01 +0300

    clone git

---
+++ script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+
+var number = 5;
+var string = "Hello!"
+var sym = Symbol();
+var boolean = true;
+null
+undefined
+var obj = {}; 
+
+console.log(4/0);
+
+let answer = confirm('Are you here?')
+
+console.log(answer);
